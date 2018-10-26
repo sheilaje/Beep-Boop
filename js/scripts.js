7dcf74845ba34d04a3a52a893ad05e2d83ba15dc
Sheila Stephen <sheila.je@gmail.com>
2018-10-26 11:32:37 -0700

    make a function that prints beep for 0

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,21 @@
-// checkNumber.forEach (function(iArray)
-// {
-// //for(int i = 0; i <= userArray.length-1 ; 1++){
-//   if(iArray.includes('0')){
-//     $("p#output").text( "Beep");
-//   }
-// });
+function checkNumber(iArray){
+  if(iArray.includes('0')){
+    //alert("Beep");
+    //$("p#output").text( "Beep");
+    return "Beep !";
+  }
+}
 
 $(document).ready(function(){
-  $("form#formid").submit(function(event){
+  $("#formid").submit(function(event){
     event.preventDefault();
-    //user-input
-    var inputNumber = $("#inputNo").val();
-    var inputArray =inputNumber.split("");
-    console.log(inputArray);
-    //checkNumber(inputArray);
-    inputArray.forEach(function(iArray){
-      if(iArray.includes('0')){
-        //alert("Beep");
-        $("p#output").text( "Beep");
-      }
+    var UserInput = $("#uinput").val();
+    var inputArray = UserInput.split("");
+    inputArray.forEach(function(inputArray){
+      //var result=changedsentence(inputArray);
+      var result=checkNumber(inputArray);
+      $("#result").text(result);
     });
+
   });
 });
-
-  // checkNumber.forEach (function(iArray)
-  // {
-  // //for(int i = 0; i <= userArray.length-1 ; 1++){
-  //   if(iArray.includes('0')){
-  //     $("p#output").text( "Beep");
-  //   }
-  // });
-//});
