3133ffe6678558e17822c1c6a5620c5b7ed2f258
Sheila Stephen <sheila.je@gmail.com>
2018-10-26 13:23:22 -0700

    make the function work for all 3 conditions

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,31 @@
-function checkNumber(iArray, rder){
-  if(iArray.includes('0')){
-    return "Beep !";
+function checkNumber(UInput){
+  var inputArray = UInput.split("");
+  var remainder = UInput % 3;
+  var includeszero = inputArray.includes("0");
+  var includesOne = inputArray.includes("1");
+  var newArray = [];
+
+  if(remainder === 0){
+    newArray.push("I'm sorry. I'm afraid I can't do that.");
+  }else
+  if(includeszero === true && includesOne ===false){
+    newArray.push("Beep!");
   }else {
-    if(iArray.includes('1')){
-      return "Boop !"
+    if (includesOne === true){
+      newArray.push("Boop !");
     }else {
-      if(rder === 0)
-      {
-        return "I'm sorry. I'm afraid I can't do that.";
-      }
+      newArray.push(UInput)
     }
   }
-}
-
+    return newArray;
+  }
 $(document).ready(function(){
   $("#formid").submit(function(event){
     event.preventDefault();
     var UserInput = $("#uinput").val();
-    var remainder = UserInput % 3;
-    //alert(remainder);
-    var inputArray = UserInput.split("");
-    inputArray.forEach(function(inputArray){
-      //var result=changedsentence(inputArray);
-      var result=checkNumber(inputArray , remainder);
+    var result=[];
+    result = checkNumber(UserInput);
       $("#result").text(result);
-    });
 
   });
 });
