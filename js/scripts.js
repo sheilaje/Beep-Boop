87869d68602b4b66acc6ffdae3db4036d00e1db5
Sheila Stephen <sheila.je@gmail.com>
2018-10-26 14:14:34 -0700

    make changes to make teh condition 30 respond as I am sorry...

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,27 @@
+//Business Logic
 function checkNumber(UInput){
   var inputArray = UInput.split("");
-  var remainder = UInput % 3;
+  var remainder = UInput % 3 === 0;
   var includeszero = inputArray.includes("0");
   var includesOne = inputArray.includes("1");
   var newArray = [];
 
-  if(remainder === 0){
+  console.log(remainder);
+  if(remainder === true && UInput >0 ){
     newArray.push("I'm sorry. I'm afraid I can't do that.");
   }else
-  if(includeszero === true && includesOne ===false){
+  if(includeszero === true && includesOne === false){
     newArray.push("Beep!");
-  }else {
+  }else
     if (includesOne === true){
       newArray.push("Boop !");
-    }else {
+    }else
+    {
       newArray.push(UInput)
     }
-  }
     return newArray;
   }
+//user interface logic
 $(document).ready(function(){
   $("#formid").submit(function(event){
     event.preventDefault();
