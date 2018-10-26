f53605681a40c18d4c8d965cf4c8fd5f91db8989
Sheila Stephen <sheila.je@gmail.com>
2018-10-26 12:05:40 -0700

    add conditions for 1 and numbers divisible by 3

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,15 @@
-function checkNumber(iArray){
+function checkNumber(iArray, rder){
   if(iArray.includes('0')){
-    //alert("Beep");
-    //$("p#output").text( "Beep");
     return "Beep !";
+  }else {
+    if(iArray.includes('1')){
+      return "Boop !"
+    }else {
+      if(rder === 0)
+      {
+        return "I'm sorry. I'm afraid I can't do that.";
+      }
+    }
   }
 }
 
@@ -10,10 +17,12 @@ $(document).ready(function(){
   $("#formid").submit(function(event){
     event.preventDefault();
     var UserInput = $("#uinput").val();
+    var remainder = UserInput % 3;
+    //alert(remainder);
     var inputArray = UserInput.split("");
     inputArray.forEach(function(inputArray){
       //var result=changedsentence(inputArray);
-      var result=checkNumber(inputArray);
+      var result=checkNumber(inputArray , remainder);
       $("#result").text(result);
     });
 
